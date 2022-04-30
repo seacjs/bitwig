74db24a9121bef926f6ece8f80005d5500ac2dfb
Solncev <vi.solnchev@gmail.com>
2022-04-30 23:42:59 +0300

    коментарий

--- src/Launchcontrol Sea.control.js
+++ src/Launchcontrol Sea.control.js
@@ -3,49 +3,28 @@ load ("Hardware.js");
 load ("TrackHandler.js");
 
 host.setShouldFailOnDeprecatedUse(true);
-host.defineController("Solncev", "Launchcontrol Sea", "0.1", "5c5f23bb-cc7a-4986-af45-3ed550a5b3c4", "Vi");
+host.defineController("Solncev", "Launchcontrol VI", "0.1", "5c5f23bb-cc7a-4986-af45-3ed550a5b3c4", "Vi");
 host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["4- Launch Control XL"], ["4- Launch Control XL"]);
 
 var hardware = null;
 var trackHandler = null;
+// var transport = null;
+
+
+
 
 function init() 
 {
    
    hardware = new LCXLHardware (host.getMidiOutPort (0), host.getMidiInPort (0), handleMidi);   
    trackHandler = new TrackHandler (host.createMainTrackBank (8, 0, 0), host.createCursorTrack ("MOXF_CURSOR_TRACK", "Cursor Track", 0, 0, true));
-   
-// Понятно почему getTrack не работал - потому что был деприкатед, теперь использовать надо getItemAt()
-//  itemCount()
-// cursorIndex()
-// getSizeOfBank()
-   // trackHandler.trackbank.cursorIndex();
-   // trackHandler.trackbank.getItemAt();
-
-   // !!!  start here 
+   // trackHandler.trackbank.getItemAt(2).select();
    // const testPos = trackHandler.cursorTrack.position();
-   //   const testPos = trackHandler.cursorTrack.selectFirstChild();
-   //   trackHandler.trackbank.getItemAt(2).select();
+      //   const testPos = trackHandler.cursorTrack
       // println('testPos: ' + testPos);
-
-   // RootControlSurfaceObject/CursorTrackProxy/TrackProxy(MOXF_CURSOR_TRACK)/ComputedIntegerValue/
-
-   // !!! ned here 
-
-      //   trackGang.getItemAt(i);
-
-
-      //   var trackGang = host.createMainTrackBank (8, 0, 0);
-      //   var cursorTrak = host.createCursorTrack ("MOXF_CURSOR_TRACK", "Cursor Track", 0, 0, true);
-
-     
-      // TransportHandler.prototype.updateLEDs = function ()
-      // {
-      //     hardware.updateLED (MOXF_BUTTON_PLAY, this.transport.isPlaying ().get ());
-      // }
-
-
+      
+   
    // transport = host.createTransport ();   
    // transport.isPlaying ().addValueObserver (function (value)  
    // {
@@ -65,11 +44,7 @@ function handleMidi (status, data1, data2)
       return;
       
 }
-
 function flush() {
-
-   // var cursorIndex = trackHandler.trackbank.cursorIndex();
-   // println('cursorIndex: ' + cursorIndex);
 
    var btns = [
       LCXL_BUTTON_1_1,
@@ -90,16 +65,15 @@ function flush() {
    {
       var track = trackHandler.trackbank.getItemAt (i);
       var volume = track.volume().get();
-     //  println('volume: ' + volume);
+      println('volume: ' + volume);
 
-      if (volume > 0) {
-         var  p = track.position ();
-         println(i + ': ' +  p.get () );
-         host.getMidiOutPort(0).sendMidi(152, btns[i], 16);
+      host.getMidiOutPort(0).sendMidi(volume > 0 ? 152 : 136, btns[i], 16);
 
-      } else {
-         host.getMidiOutPort(0).sendMidi(136, btns[i], 16);
-      }
+      // check start
+
+      // println('select: '+ select);
+      // check end
+
    }
 
    if (trackHandler?.cursorTrack?.track) {
@@ -107,9 +81,8 @@ function flush() {
 
    }
    println('---' + trackHandler.cursorTrack.mute().get());
-
+ 
 }
-
 function exit() {
    //println()
 }
